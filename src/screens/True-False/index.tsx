872a8167f03695ce55208cf10fea77d453861664
nijat <nicatveliyev@gmail.com>
2023-12-21 15:02:04 +0400

    true false

--- src/screens/True-False/index.tsx
+++ src/screens/True-False/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { ImageBackground } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-function truefalse(){
-    return(
-        <ImageBackground source={require('../../assets/images/background.png')}>
-            <SafeAreaView>
-                
-            </SafeAreaView>
-        </ImageBackground>
+function TrueFalseScreen() {
+    return (
+        <SafeAreaView>
+            <ImageBackground source={require('../../assets/images/background.png')}>
+
+            </ImageBackground>
+        </SafeAreaView>
     )
 }
 
-export default truefalse
+export default TrueFalseScreen
