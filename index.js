c17c3164119a1695321d4b627ba4933dee54f313
Mahammad2003 <mahammadasadli2003@gmail.com>
2023-12-22 14:20:47 +0400

    True-False Screen

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import TrueFalseScreen from './src/screens/True-False';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TrueFalseScreen);
